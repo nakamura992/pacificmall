86035829226a2e16e99bf873f7df21b0244c2eb7
nakamura992 <ryu1228846@gmail.com>
2023-07-03 16:29:17 +0900

    7/3

---
+++ index.php
@@ -0,0 +1,53 @@
+<?php get_header(); ?>
+    <section class="section-contents" id="news">
+      <div class="wrapper">
+        <span class="section-title-en">News Release</span>
+        <h2 class="section-title">ニュースリリース</h2>
+        <p class="section-lead">パシフィックモール開発の最新情報をお送りします</p>
+        <ul class="news">
+          <li class="news-item">
+            <a class="detail-link" href="#">
+              <time class="time">2023.01.14</time>
+              <p class="title">インド 消費者向けアプリのMecha-Indiaと業務提携開始</p>
+              <p class="news-text">パシフィックモール株式会社（以下、当社）は、インド共和国（以下、インド）において、Mecha-India（以下、メカ・インディア）との協業を開始します。 Mecha-Indiaは、インドにおける配車サービス大手であり、電 [&hellip;]</p>
+            </a>
+          </li>
+          <li class="news-item">
+            <a class="detail-link" href="#">
+              <time class="time">2022.12.17</time>
+              <p class="title">年末年始休業のお知らせ</p>
+              <p class="news-text">お客様　各位 師走の候、貴社ますますご清祥のこととお喜び申し上げます。 平素は格別のご高配を賜り、厚くお礼申し上げます。 さて、本年（令和3年）の年末年始は、下記の通り休業させていただきますので、よろしくお願いいたしま [&hellip;]</p>
+            </a>
+          </li>
+          <li class="news-item">
+            <a class="detail-link" href="#">
+              <time class="time">2022.11.15</time>
+              <p class="title">米国ニューヨーク・パークアベニューモール 美術館 「The Art of Park Ave.」にて、特設イベント「世界の若手美術展」を開催</p>
+              <p class="news-text">美術館「The Art of Park Ave.」は、若手芸術家の作品を展示している美術館としてはニューヨーク最大規模で、常時約5,000点の作品を展示しています。 この度、2021年11月30日&#12316;2022年1月31日の [&hellip;]</p>
+            </a>
+          </li>
+        </ul>
+        <div class="section-buttons">
+          <button type="button" class="button button-ghost" onclick="javascript:location.href = '#';">
+            ニュースリリース一覧を見る
+          </button>
+        </div>
+      </div>
+    </section>
+    <section class="section-contents" id="company">
+      <div class="wrapper">
+        <span class="section-title-en">Corporate Information</span>
+        <h2 class="section-title">企業情報</h2>
+        <p class="section-lead">
+          私たちパシフィックモール開発は、<br />
+          ショッピングモール開発を通じて新たな価値を創造し<br />
+          社会に貢献するグローバルな企業を目指します。
+        </p>
+        <div class="section-buttons">
+          <button type="button" class="button button-ghost" onclick="javascript:location.href = '#';">
+            企業情報を見る
+          </button>
+        </div>
+      </div>
+    </section>
+<?php get_footer(); ?>
